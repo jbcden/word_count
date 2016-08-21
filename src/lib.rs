32d15e3a8a26f15843b3a77f8af852dd804ff29a
Jacob Chae <jbcden@gmail.com>
2016-08-21 11:34:16 -0400

    Tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ mod word_counter {
     use std::collections::BTreeMap;
     use regex::Regex;
 
-    pub fn count(str: &str) -> BTreeMap<&str, i32> {
+    pub fn count(str: &str) -> BTreeMap<String, i32> {
         let mut map = BTreeMap::new();
-        let re = Regex::new(r"[ ,:!&@$%^&]+").unwrap();
+        let re = Regex::new(r"[ _.#,:!&@$%^&]+").unwrap();
         let words = re.split(str);
 
-        for word in words {
+        for word in words.filter(|c| !c.is_empty()) {
+            let word = word.to_lowercase();
+
             let value = match map.get(&word) {
                 Some(val) => val + 1,
                 None => 1,
@@ -33,7 +35,7 @@ mod tests {
         let str = "test";
 
         let mut map = BTreeMap::new();
-        map.insert(str, 1);
+        map.insert(str.to_string(), 1);
 
         assert_eq!(word_counter::count(str), map);
     }
@@ -43,9 +45,9 @@ mod tests {
         let str = "each one once";
 
         let mut map = BTreeMap::new();
-        map.insert("each", 1);
-        map.insert("one", 1);
-        map.insert("once", 1);
+        map.insert("each".to_string(), 1);
+        map.insert("one".to_string(), 1);
+        map.insert("once".to_string(), 1);
 
         assert_eq!(word_counter::count(str), map);
     }
@@ -55,11 +57,11 @@ mod tests {
         let str = "one fish two fish red fish blue fish";
 
         let mut map = BTreeMap::new();
-        map.insert("one", 1);
-        map.insert("fish", 4);
-        map.insert("two", 1);
-        map.insert("red", 1);
-        map.insert("blue", 1);
+        map.insert("one".to_string(), 1);
+        map.insert("fish".to_string(), 4);
+        map.insert("two".to_string(), 1);
+        map.insert("red".to_string(), 1);
+        map.insert("blue".to_string(), 1);
 
         assert_eq!(word_counter::count(str), map);
     }
@@ -69,11 +71,11 @@ mod tests {
         let str = "car : carpet as java : javascript!!&@$%^&";
 
         let mut map = BTreeMap::new();
-        map.insert("car", 1);
-        map.insert("carpet", 1);
-        map.insert("as", 1);
-        map.insert("java", 1);
-        map.insert("javascript", 1);
+        map.insert("car".to_string(), 1);
+        map.insert("carpet".to_string(), 1);
+        map.insert("as".to_string(), 1);
+        map.insert("java".to_string(), 1);
+        map.insert("javascript".to_string(), 1);
 
         assert_eq!(word_counter::count(str), map);
     }
@@ -83,9 +85,45 @@ mod tests {
         let str = "testing, 1, 2 testing";
 
         let mut map = BTreeMap::new();
-        map.insert("testing", 2);
-        map.insert("1", 1);
-        map.insert("2", 1);
+        map.insert("testing".to_string(), 2);
+        map.insert("1".to_string(), 1);
+        map.insert("2".to_string(), 1);
+
+        assert_eq!(word_counter::count(str), map);
+    }
+
+    #[test]
+    fn normalize_case() {
+        let str = "go Go GO";
+
+        let mut map = BTreeMap::new();
+        map.insert("go".to_string(), 3);
+
+        assert_eq!(word_counter::count(str), map);
+    }
+
+    #[test]
+    fn prefix_punctuation() {
+        let str = "!%%#testing, 1, 2 testing";
+
+        let mut map = BTreeMap::new();
+        map.insert("testing".to_string(), 2);
+        map.insert("1".to_string(), 1);
+        map.insert("2".to_string(), 1);
+
+        assert_eq!(word_counter::count(str), map);
+    }
+
+    #[test]
+    fn symbols_are_separators() {
+        let str = "hey,my_spacebar_is_broken.";
+
+        let mut map = BTreeMap::new();
+        map.insert("hey".to_string(), 1);
+        map.insert("my".to_string(), 1);
+        map.insert("spacebar".to_string(), 1);
+        map.insert("is".to_string(), 1);
+        map.insert("broken".to_string(), 1);
 
         assert_eq!(word_counter::count(str), map);
     }
